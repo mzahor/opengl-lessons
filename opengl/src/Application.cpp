3b34109485728cdf6c921edc62d42e86c0d16b96
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2021-03-10 22:45:09 +0200

    feat: rect with indices

--- opengl/src/Application.cpp
+++ opengl/src/Application.cpp
@@ -7,31 +7,35 @@
 
 #define LOG(x) std::cout << x << std::endl
 
-
-int loadFile(const char* filename, std::string& src) {
+int loadFile(const char *filename, std::string &src)
+{
 	std::ifstream file(filename, std::ios_base::in);
-	if (!file) {
+	if (!file)
+	{
 		std::cout << "Failed to open file " << filename << std::endl;
 		return 1;
 	}
 	file.seekg(0, std::ios::end);
-	src.reserve(file.tellg());
+	src.reserve((unsigned int)file.tellg());
 	file.seekg(0, std::ios::beg);
 	src.assign((std::istreambuf_iterator<char>(file)),
-				std::istreambuf_iterator<char>());
+			   std::istreambuf_iterator<char>());
 	file.close();
 	return 0;
 }
 
-struct ShaderSource {
+struct ShaderSource
+{
 	std::string VertexSource;
 	std::string FragmentSource;
 };
 
-ShaderSource ParseShader(const char* fname) {
+ShaderSource ParseShader(const char *fname)
+{
 	std::ifstream file(fname);
 	std::string line;
-	enum class ShaderType {
+	enum class ShaderType
+	{
 		NONE = -1,
 		VERTEX = 0,
 		FRAGMENT = 1,
@@ -41,14 +45,20 @@ ShaderSource ParseShader(const char* fname) {
 	ShaderType type = ShaderType::NONE;
 	while (getline(file, line))
 	{
-		if (line.find("#shader") != std::string::npos) {
-			if (line.find("vertex") != std::string::npos) {
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+			{
 				type = ShaderType::VERTEX;
-			}  else {
+			}
+			else
+			{
 				type = ShaderType::FRAGMENT;
 			}
 			continue;
-		} else if (type == ShaderType::NONE) {
+		}
+		else if (type == ShaderType::NONE)
+		{
 			continue;
 		}
 		ss[(int)type] << line << "\n";
@@ -60,19 +70,21 @@ ShaderSource ParseShader(const char* fname) {
 	return source;
 }
 
-unsigned int compileShader(const std::string& src, GLenum type) {
+unsigned int compileShader(const std::string &src, GLenum type)
+{
 	unsigned int sid = glCreateShader(type);
-	const char* source = src.c_str();
+	const char *source = src.c_str();
 	glShaderSource(sid, 1, &source, nullptr);
 	glCompileShader(sid);
 
 	int success = GL_FALSE;
 	glGetShaderiv(sid, GL_COMPILE_STATUS, &success);
 
-	if (success == GL_FALSE) {
+	if (success == GL_FALSE)
+	{
 		int logSize = 0;
 		glGetShaderiv(sid, GL_INFO_LOG_LENGTH, &logSize);
-		char* errMsg = (char*)alloca(logSize);
+		char *errMsg = (char *)alloca(logSize);
 		glGetShaderInfoLog(sid, logSize, &logSize, errMsg);
 		glDeleteShader(sid);
 		std::cout << errMsg << std::endl;
@@ -82,7 +94,8 @@ unsigned int compileShader(const std::string& src, GLenum type) {
 	return sid;
 }
 
-unsigned int createProgram(const std::string& vertexShader, const std::string& fragmentShader) {
+unsigned int createProgram(const std::string &vertexShader, const std::string &fragmentShader)
+{
 	unsigned int pid = glCreateProgram();
 	unsigned int v_sid = compileShader(vertexShader, GL_VERTEX_SHADER);
 	unsigned int f_sid = compileShader(fragmentShader, GL_FRAGMENT_SHADER);
@@ -93,10 +106,11 @@ unsigned int createProgram(const std::string& vertexShader, const std::string& f
 	int success = GL_FALSE;
 	glGetProgramiv(pid, GL_LINK_STATUS, &success);
 
-	if (!success) {
+	if (!success)
+	{
 		int logSize = 0;
 		glGetProgramiv(pid, GL_INFO_LOG_LENGTH, &logSize);
-		char* errMsg = (char*)alloca(logSize);
+		char *errMsg = (char *)alloca(logSize);
 		glGetProgramInfoLog(pid, logSize, &logSize, errMsg);
 		glDeleteProgram(pid);
 		std::cout << errMsg << std::endl;
@@ -111,13 +125,14 @@ unsigned int createProgram(const std::string& vertexShader, const std::string& f
 
 int main(void)
 {
-	GLFWwindow* window;
+	GLFWwindow *window;
 
-	/* Initialize the library */
 	if (!glfwInit())
+	{
+		LOG("Failed to init glfw");
 		return -1;
+	}
 
-	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
 	{
@@ -126,58 +141,63 @@ int main(void)
 		return -1;
 	}
 
-	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
-	if (glewInit() != GLEW_OK) {
+	if (glewInit() != GLEW_OK)
+	{
 		LOG("Failed to init glew");
 	}
-	else {
+	else
+	{
 		LOG(glGetString(GL_VERSION));
 	}
 
-	float buf[] = {
-		-0.5f, -0.5f,
+	float vBuf[] = {
+		-0.5f,-0.5f,
 		0.5f, -0.5f,
 		0.5f, 0.5f,
-
-		-0.5f, -0.5f,
 		-0.5f, 0.5f,
-		0.5f, 0.5f,
 	};
 
-	GLuint buffer;
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), buf, GL_STATIC_DRAW);
+	unsigned int iBuf[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	unsigned int vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), vBuf, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 	glEnableVertexAttribArray(0);
+
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), iBuf, GL_STATIC_DRAW);
 
 	int err = 0;
 	std::string vertexShader;
 	err = loadFile("./VertexShader.glsl", vertexShader);
-	if (err) return err;
+	if (err)
+		return err;
 	std::string fragmentShader;
 	err = loadFile("./FragmentShader.glsl", fragmentShader);
-	if (err) return err;
+	if (err)
+		return err;
 	auto prog_id = createProgram(vertexShader, fragmentShader);
 
-	/*auto shaders = ParseShader("./Basic.shader");
-	auto prog_id = createProgram(shaders.VertexSource, shaders.FragmentSource);*/
+	// auto shaders = ParseShader("./Basic.shader");
+	// auto prog_id = createProgram(shaders.VertexSource, shaders.FragmentSource);
 
 	glUseProgram(prog_id);
 
-
-	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
-		glDrawArrays(GL_TRIANGLES, 0, 3 * 2);
-
-		/* Swap front and back buffers */
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
