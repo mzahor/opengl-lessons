598ca8f7bfd33d840d3d76de8ccfcd915b44eb4d
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2021-03-20 00:15:18 +0200

    feat: logo behind

--- opengl/src/Application.cpp
+++ opengl/src/Application.cpp
@@ -119,16 +119,16 @@ int main(void)
 				static float f = 0.0f;
 				static int counter = 0;
 
-				ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+				ImGui::Begin("Hello, world!");
 
-				ImGui::Text("This is some useful text.");		   // Display some text (you can use a format strings too)
-				ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
+				ImGui::Text("This is some useful text.");
+				ImGui::Checkbox("Demo Window", &show_demo_window);
 				ImGui::Checkbox("Another Window", &show_another_window);
 
-				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);			 // Edit 1 float using a slider from 0.0f to 1.0f
-				ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
+				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+				ImGui::ColorEdit3("clear color", (float *)&clear_color);
 
-				if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
+				if (ImGui::Button("Button"))
 					counter++;
 				ImGui::SameLine();
 				ImGui::Text("counter = %d", counter);
@@ -137,10 +137,9 @@ int main(void)
 				ImGui::End();
 			}
 
+			renderer.Draw(vao, ibo, shader);
 			ImGui::Render();
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-			renderer.Draw(vao, ibo, shader);
 
 			GLCall(glfwSwapBuffers(window));
 			GLCall(glfwPollEvents());
